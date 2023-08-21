0ef88e8d88def2c2835eb53e83cefd53339e9fec
rileygaffney3-NKLStC <rileygaffney3@icloud.com>
2023-08-21 15:44:07 -0300

    attempting to update JS

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 //calling our variables 
-var startButton = "Start"
+var phrase = "Hi my name is riley";  //string variable <----
 
-const event = new Date('August 19, 1975 23:15:30');
-
-event.setSeconds(42);
-
-console.log(event.getSeconds());
-// Expected output: 42
-
-console.log(event);
-// Expected output: "Sat Apr 19 1975 23:15:42 GMT+0100 (CET)"
-// Note: your timezone may vary
- 
+document.write( phrase ); 
