cc1c5fa9a0e5387a23fd615b7d021a936c7bdd25
rileygaffney3-NKLStC <rileygaffney3@icloud.com>
2023-08-25 22:47:16 -0300

    attempted to make stop button function

--- script.js
+++ script.js
@@ -2,25 +2,30 @@
 var timer; 
 var ele = document.getElementById('timer')
 
-
-
-var winCounter = 0;
-var loseCounter = 0;
-var timer;
-var timerCount;
-var timerWin
-
 var answers = ["A","B","C"];
 
 //setting the timer 
 
-(function (){
+var startBtn = document.getElementsByClassName("start-button");
+startBtn[0].addEventListener("start", function(){
+    console.log("started")
+})
+
+var stopBtn = document.getElementsByClassName("stop-button");
+stopBtn[0].addEventListener('click', function(){
+    console.log("stopped")
+})
+
+function startQuiz(){
     var sec = 0; 
     timer = setInterval(()=>{
         ele.innerHTML = '00:'+sec;
         sec ++; 
     }, 1000)
-})()
+}
+
+
+
 
 function pause(){
     clearInterval(timer); 
@@ -52,9 +57,6 @@ function correctAnswer() {
 //event listeners
 
 
-
-
-document.getElementById("secs").innerHTML = seconds + "s"
 document.getElementById('button').onclick = function() {
 
 
@@ -62,5 +64,9 @@ document.getElementById('button').onclick = function() {
 
 
 
-
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
 
